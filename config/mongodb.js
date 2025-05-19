5d4e1c642fe9158c0fd3c357d19fc6027b14a2be
Ramkantodey <ramkantodey@gmail.com>
2025-05-19 18:52:02 +0600

    done backend

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,19 +1,12 @@
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
     mongoose.connection.on('connected', () => {
-        console.log('MongoDB connected successfully');
+
     });
 
-    mongoose.connection.on('error', (err) => {
-        console.error('MongoDB connection error:', err);
-    });
-
-    // এখানে শুধু process.env.MONGODB_URI ব্যবহার করবে, ডাটাবেজ নাম .env এ দেওয়া আছে
-    await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    await mongoose.connect(`${process.env.MONGODB_URI}/imagify`);
 };
 
 export default connectDB;
